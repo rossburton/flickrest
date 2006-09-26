29adee370f54938a36f16102ea45591cd1854b27
Ross Burton <ross@burtonini.com>
2006-09-26 20:27:13 +0100

    Split test code out

--- flickrpc.py
+++ flickrpc.py
@@ -1,6 +1,4 @@
-#! /usr/bin/python
-
-from twisted.internet import reactor, defer
+from twisted.internet import defer
 from twisted.web.xmlrpc import Proxy
 from elementtree import ElementTree
 import os, md5
@@ -85,19 +83,3 @@ class FlickRPC:
         # TODO: chain up the error callbacks too
         flickr.auth_getFrob().addCallback(gotFrob)
         return d
-
-
-if __name__ == "__main__":
-    flickr = FlickRPC("c53cebd15ed936073134cec858036f1d", "7db1b8ef68979779", "read")
-    def connected(authenticated):
-        def gotInfo(p):
-            print "Got photo title '%s'" % p.find("title").text
-        def gotFavs(p):
-            print "Got favourites:"
-            for photo in p.findall("photo"):
-                print "  %s" % photo.get('title')        
-        flickr.favorites_getList().addCallback(gotFavs)
-        flickr.photos_getInfo(photo_id="209423026").addCallback(gotInfo)
-
-    flickr.authenticate().addCallback(connected)
-    reactor.run()
