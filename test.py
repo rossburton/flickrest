df47fe767d618275f9e2193fea2c9e780c9eaaa9
Ross Burton <ross@burtonini.com>
2006-12-16 17:34:06 +0000

    Add upload example

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from twisted.internet import reactor
 from flickrest import Flickr
 
 if __name__ == "__main__":
-    flickr = Flickr("c53cebd15ed936073134cec858036f1d", "7db1b8ef68979779", "read")
+    flickr = Flickr("c53cebd15ed936073134cec858036f1d", "7db1b8ef68979779", "write")
     def connected(authenticated):
         def gotInfo(p):
             print "Got photo title '%s'" % p.find("photo/title").text
@@ -13,7 +13,10 @@ if __name__ == "__main__":
             print "Got favourites:"
             for photo in p.findall("photos/photo"):
                 print "  %s" % photo.get('title')
-        flickr.favorites_getList().addCallback(gotFavs)
+        def uploaded(p):
+            print p
         flickr.photos_getInfo(photo_id="209423026").addCallback(gotInfo)
+        #flickr.favorites_getList().addCallback(gotFavs)
+        #flickr.upload(filename="/home/ross/Pictures/Photos/Artwork/snow-grass.jpg").addCallback(uploaded)
     flickr.authenticate().addCallback(connected)
     reactor.run()
